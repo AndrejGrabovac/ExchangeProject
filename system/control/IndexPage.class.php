d18ee3a06e7fdcfff5f8ca0f53f9c3bc1d347369
AndrejGrabovac <101126091+AndrejGrabovac@users.noreply.github.com>
2022-09-15 01:49:31 +0200

    90% done

--- system/control/IndexPage.class.php
+++ system/control/IndexPage.class.php
@@ -6,18 +6,18 @@ class IndexPage extends AbstractPage
         $this->template = 'currency';
 
         $status = "
-        http://localhost/PROJEKT.v1.4/index.php?page=Currency " . "<br>" . "
+        http://localhost/ExchangeProject/index.php?page=Currency " . "<br>" . "
         " . "<br><br><br>" . "
-        http://localhost/PROJEKT.v1.4/index.php?page=ReadCurrencies " . "<br>" . "
-        http://localhost/PROJEKT.v1.4/index.php?page=CreateCurrency&.currency=EUR " . "<br>" . "
-        http://localhost/PROJEKT.v1.4/index.php?page=ReadCurrency&.currency=EUR " . "<br>" . "
-        http://localhost/PROJEKT.v1.4/index.php?page=DeleteCurrency&.currency=EUR " . "<br>" . "
-        http://localhost/PROJEKT.v1.4/index.php?page=RateSelected&.currencyCode=EUR " . "<br>" . "
-        http://localhost/PROJEKT.v1.4/index.php?page=Convert&.currencyOne=USD&.currencyTwo=EUR&.amount=500" . "<br>" . "
+        http://localhost/ExchangeProject/index.php?page=ReadCurrencies " . "<br>" . "
+        http://localhost/ExchangeProject/index.php?page=CreateCurrency&.currency=EUR " . "<br>" . "
+        http://localhost/ExchangeProject/index.php?page=ReadCurrency&.currency=EUR " . "<br>" . "
+        http://localhost/ExchangeProject/index.php?page=DeleteCurrency&.currency=EUR " . "<br>" . "
+        http://localhost/ExchangeProject/index.php?page=RateSelected&.currencyCode=EUR " . "<br>" . "
+        http://localhost/ExchangeProject/index.php?page=Convert&.currencyOne=USD&.currencyTwo=EUR&.amount=500" . "<br>" . "
         <br><br>
-        http://localhost/PROJEKT.v1.4/index.php?page=Rate " . "<br>" . "
-        http://localhost/PROJEKT.v1.4/index.php?page=HistoryByDate&.history=2022-04-13" . "<br>" . "
-        http://localhost/PROJEKT.v1.4/index.php?page=History
+        http://localhost/ExchangeProject/index.php?page=Rate " . "<br>" . "
+        http://localhost/ExchangeProject/index.php?page=HistoryByDate&.history=2022-04-13" . "<br>" . "
+        http://localhost/ExchangeProject/index.php?page=History
         ";
 
         $this->v['var'] = $status;
